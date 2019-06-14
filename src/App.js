33ddd10dcbea33ef053536f2594ea78137e1983d
Albert Stjärne <albert.stjarne@gmail.com>
2019-06-14 13:51:35 +0200

    adds getUser function with get request towards a single github user

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -21,6 +22,15 @@ class App extends Component {
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
     this.setState({ users: res.data.items, loading: false});
+  }
+  
+  // Get single github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  
+    this.setState({ user: res.data, loading: false});
+
   }
 
   // Clear users from state
