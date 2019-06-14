63613e7008b0bef616c1a43452e4d876e5b5816c
Albert Stjärne <albert.stjarne@gmail.com>
2019-06-14 13:43:52 +0200

    importing Link and adding links in Navbar to Home and About

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({ icon, title }) => {
@@ -8,6 +9,12 @@ const Navbar = ({ icon, title }) => {
         <h1>
           <i className={icon}></i> {title}
         </h1>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </nav>
     )
 }
