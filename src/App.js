b9b358539e7cf30c611b6f1e1a3b07b60337c539
Albert Stjärne <albert.stjarne@gmail.com>
2019-06-17 18:05:04 +0200

    adds NotFound component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -32,6 +31,7 @@ const App = () => {
                 path='/user/:login' 
                 component={User} 
               />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </div>
